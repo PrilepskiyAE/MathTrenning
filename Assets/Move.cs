6b0cc5ed42a825e7a91500357ac633285a88fe44
Prilepskiy_AE <alexey.prilepskiy1992@gmail.com>
2026-02-23 15:09:46 +0700

    Lerp

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -18,8 +18,8 @@ public class Move : MonoBehaviour
  void FixedUpdate() 
     {
         float horizontal = Input.GetAxis("Horizontal");
-        //float vertical = Input.GetAxis("Vertical");
-        rb.linearVelocity = new Vector3(horizontal*speed,0f,0f);
+        float vertical = Input.GetAxis("Vertical");
+        rb.linearVelocity = new Vector3(horizontal*speed,0f,vertical*speed);
         stepsAudio.volume = Mathf.Abs(horizontal);
     }
 }
